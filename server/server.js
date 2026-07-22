9d006ee96f5a94026550449a02b535ef12097fb8
@Ruchi012 <iruchisingh0202@gmail.com>
2026-07-22 21:50:09 +0530

    server updated

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-  "https://your-frontend.vercel.app"
+  "https://ai-resume-repo.vercel.app"
   ],
   credentials: true
 }));
