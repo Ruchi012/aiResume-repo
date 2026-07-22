488f5e731e2bd9493b0eed99df66a8d07131aeca
@Ruchi012 <iruchisingh0202@gmail.com>
2026-07-22 21:17:05 +0530

    changes update

--- server/server.js
+++ server/server.js
@@ -17,7 +17,10 @@ await connectDB()
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+  "https://your-frontend.vercel.app"
+  ],
   credentials: true
 }));
 
